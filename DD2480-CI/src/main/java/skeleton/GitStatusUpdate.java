1f2d6298f3e17daff15ea4212c31e0715b4d962c
Filippa Nilsson <filnils@kth.se>
2024-02-10 15:58:54 +0100

    Solve bug in the commit description update

--- DD2480-CI/src/main/java/skeleton/GitStatusUpdate.java
+++ DD2480-CI/src/main/java/skeleton/GitStatusUpdate.java
@@ -3,25 +3,22 @@ package skeleton;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 /**
  * Updates the commit status on the repository to one of four keywords - success, failure, error, and pending
- * by connecting to the GitHub API
+ * by connecting to the GitHub API.
  * */
 public class GitStatusUpdate {
     private final BuildStatus buildStatus;
     private final String gitUri;
     private final String sha;
     private final String githubToken;
-    private CloseableHttpClient httpClient; // Added member variable for CloseableHttpClient
 
 
     /**
@@ -34,27 +31,33 @@ public class GitStatusUpdate {
         this.sha = sha;
         this.buildStatus = buildStatus;
         this.gitUri = "https://api.github.com/";
-        this.githubToken = "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";
+        this.githubToken = System.getenv("ci_token"); // make sure this works
     }
 
     /**
      * Sets the status of the commit by changing values of the description, status and context.
      *
      * @param json The JSON object that contains the values to be sent to the GitHub API
+     * @return A JSON object containing the commit status and other information
      */
     private JSONObject statusSetter(JSONObject json) {
+
         switch (buildStatus) {
             case SUCCESS:
-                json.put("description", "Build success");
+                json.put("description", "Build succeeded");
+                break;
             case FAILURE:
                 json.put("description", "Build failed");
+                break;
             case PENDING:
                 json.put("description", "Build pending");
+                break;
             default:
                 json.put("description", "Build error");
+                break;
         }
         json.put("state", buildStatus.toString());
-        json.put("context", "Group 12");
+        json.put("context", "dd2480-CI");
 
         return json;
     }
@@ -62,29 +65,30 @@ public class GitStatusUpdate {
     /**
      * Creates a HTTP POST request to update the commit status.
      * Throws an exception if the POST request fails.
+     * @param servletResponse The response that we send a message to if the API POST request succeeded
      */
-    public void updateStatus(HttpServletResponse servletResponse) throws IOException {
-        JSONObject json = statusSetter(new JSONObject());
-
-        HttpClient httpClient = HttpClients.createDefault();
-        String apiUrl = gitUri + "repos/filippanilsson/dd2480-CI/statuses/" + sha;
-
-        HttpPost httpPost = new HttpPost(apiUrl);
-
-        httpPost.setHeader("Authorization", "Bearer " + githubToken);
-        httpPost.setHeader("Content-Type", "application/json");
-
-        HttpResponse response;
+    public void updateStatus(HttpServletResponse servletResponse)  {
         try {
+            JSONObject json = statusSetter(new JSONObject());
+
+            HttpClient httpClient = HttpClients.createDefault();
+            String apiUrl = gitUri + "repos/filippanilsson/dd2480-CI/statuses/" + sha;
+
+            HttpPost httpPost = new HttpPost(apiUrl);
+
+            httpPost.setHeader("Authorization", "Bearer " + githubToken);
+            httpPost.setHeader("Content-Type", "application/json");
+
             StringEntity entity = new StringEntity(json.toString());
             httpPost.setEntity(entity);
-            response = httpClient.execute(httpPost);
-            System.out.println("Response status code: " + response.getStatusLine().getStatusCode());
+            HttpResponse response = httpClient.execute(httpPost);
+
+            servletResponse.getWriter().println("Response status code: " + response.getStatusLine().getStatusCode() + "\n");
+            servletResponse.getWriter().println("Status updated \n");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        servletResponse.getWriter().println("Status updated");
     }
 
 }
