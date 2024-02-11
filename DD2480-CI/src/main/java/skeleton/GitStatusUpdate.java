2c7a296942b6aa23142e620c3adbcb3e8884e3ae
Filippa Nilsson <75281470+filippanilsson@users.noreply.github.com>
2024-02-11 17:48:10 +0100

    Feat: Update commit status on GitHub (#19)
    * Fix #11: Implement GitStatusUpdate class
    
    Add test class
    
    * Minor bugfixing
    
    * Major refactoring and bugfixing
    
    * Solve bug in the commit description update
    
    * Refactor GitStatusUpdateTest (#18)
    
    Minor changes to GitStatusUpdate and ContinuosIntegration to remove the httpservletresponse
    
    * Refactoring of files after merge from child branch
    
    Removal of method calls in ContinuousIntegrationServer class
    
    * Add requested changes
    
    * Insert correct port number to the server
    
    ---------
    
    Co-authored-by: Filippa Nilsson <filnils@kth.se>
    Co-authored-by: LottaJohnsson <35195355+LottaJohnsson@users.noreply.github.com>

---
+++ DD2480-CI/src/main/java/skeleton/GitStatusUpdate.java
@@ -0,0 +1,92 @@
+package skeleton;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.json.JSONObject;
+
+/**
+ * Updates the commit status on the repository to one of four keywords - success, failure, error, and pending
+ * by connecting to the GitHub API.
+ * */
+public class GitStatusUpdate {
+    private final BuildStatus buildStatus;
+    private final String gitUri;
+    private final String sha;
+    private final String githubToken;
+
+    /**
+     * Constructor for the GitStatusUpdate class
+     *
+     * @param sha         The SHA id for the commit
+     * @param buildStatus The status of the finished build
+     */
+    public GitStatusUpdate(String sha, BuildStatus buildStatus) {
+        this.sha = sha;
+        this.buildStatus = buildStatus;
+        this.gitUri = "https://api.github.com/";
+        this.githubToken = System.getenv("ci_token"); // make sure this works
+    }
+
+    /**
+     * Sets the status of the commit by changing values of the description, status and context.
+     *
+     * @param json The JSON object that contains the values to be sent to the GitHub API
+     * @return A JSON object containing the commit status and other information
+     */
+    private JSONObject statusSetter(JSONObject json) {
+
+        switch (buildStatus) {
+            case SUCCESS:
+                json.put("description", "Build succeeded");
+                break;
+            case FAILURE:
+                json.put("description", "Build failed");
+                break;
+            case PENDING:
+                json.put("description", "Build pending");
+                break;
+            default:
+                json.put("description", "Build error");
+                break;
+        }
+        json.put("state", buildStatus.toString());
+        json.put("context", "dd2480-CI");
+
+        return json;
+    }
+
+    /**
+     * Creates a HTTP POST request to update the commit status.
+     * Throws an exception if the POST request fails.
+     */
+    public void updateStatus()  {
+
+        try {
+            JSONObject json = statusSetter(new JSONObject());
+
+            HttpClient httpClient = HttpClients.createDefault();
+            String apiUrl = gitUri + "repos/filippanilsson/dd2480-CI/statuses/" + sha;
+
+            HttpPost httpPost = new HttpPost(apiUrl);
+
+            httpPost.setHeader("Authorization", "Bearer " + githubToken);
+            httpPost.setHeader("Content-Type", "application/json");
+
+            StringEntity entity = new StringEntity(json.toString());
+            httpPost.setEntity(entity);
+            HttpResponse response = httpClient.execute(httpPost);
+
+            if (response.getStatusLine().getStatusCode() != 201){
+                throw new Error("Status update could not be completed, response status "+ response.getStatusLine().getStatusCode());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
---
+++ DD2480-CI/src/test/java/skeleton/GitStatusUpdateTest.java
@@ -0,0 +1,33 @@
+package skeleton;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for the GitStatusUpdate class
+ * */
+public class GitStatusUpdateTest {
+
+    private GitStatusUpdate gitStatusUpdate;
+    private BuildStatus buildStatus;
+
+    @BeforeEach
+    void setUp() {
+        buildStatus = BuildStatus.SUCCESS;
+        gitStatusUpdate = new GitStatusUpdate("5d3b4dc5b80c09cb27f13e1b6a846188dddc9cf0", buildStatus);
+    }
+
+    @Test
+    public void assertThatValidInputDoesNotThrowError(){
+        assertDoesNotThrow(() -> gitStatusUpdate.updateStatus());
+    }
+
+
+    @Test
+    public void assertThatInvalidShaThrowsError(){
+        gitStatusUpdate = new GitStatusUpdate("123", buildStatus);
+        assertThrows(Error.class, () -> gitStatusUpdate.updateStatus());
+    }
+
+}
