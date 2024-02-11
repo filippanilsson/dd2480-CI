c8418e294b0c4ad14abc1913e0d01a709ff88838
Filippa Nilsson <filnils@kth.se>
2024-02-11 17:36:23 +0100

    Insert correct port number to the server

--- DD2480-CI/src/main/java/skeleton/ContinuousIntegrationServer.java
+++ DD2480-CI/src/main/java/skeleton/ContinuousIntegrationServer.java
@@ -36,7 +36,7 @@ public class ContinuousIntegrationServer extends AbstractHandler
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
     {
-        Server server = new Server(8080);
+        Server server = new Server(8012);
         server.setHandler(new ContinuousIntegrationServer());
         server.start();
         server.join();
